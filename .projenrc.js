aeda72ec931377bad268ba3e7848e55880c23a0a
Renat <rzhilkibaev@gmail.com>
2022-03-29 23:30:50 -0700

    Update to node 16

--- .projenrc.js
+++ .projenrc.js
@@ -1,8 +1,9 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.18.0',
   defaultReleaseBranch: 'main',
   name: 'infra',
+  minNodeVersion: '16.14.0',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
