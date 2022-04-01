82cbb1fb572532efe4ca3a6d340b5e65970121b8
Renat <rzhilkibaev@gmail.com>
2022-03-31 21:19:00 -0700

    Upgrade CDK

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,6 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.18.0',
+  cdkVersion: '2.19.0',
   defaultReleaseBranch: 'main',
   name: 'infra',
   minNodeVersion: '16.14.0',
